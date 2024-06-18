0712ca1196649c7de41ab40af110470ebf678036
Manya Singh <manyasingh23@yahoo.com>
2024-06-18 22:35:08 +0530

    Add files via upload

---
+++ trainer.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from datasets import load_dataset, Dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+
+# Load and preprocess your dataset
+data = {'text': ["I love this!", "I hate this!"], 'label': [1, 0]}
+df = pd.DataFrame(data)
+dataset = Dataset.from_pandas(df)
+
+# Load tokenizer and model
+model_name = "distilbert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+# Tokenize the data
+def tokenize_function(examples):
+    return tokenizer(examples['text'], padding="max_length", truncation=True)
+
+tokenized_datasets = dataset.map(tokenize_function, batched=True)
+
+# Training arguments
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    num_train_epochs=3,
+    weight_decay=0.01,
+)
+
+# Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_datasets,
+    eval_dataset=tokenized_datasets,
+)
+
+# Train the model
+trainer.train()

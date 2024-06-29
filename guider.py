c76e2ccdb0b5216add9226793cd29126cfb4387b
Manya Singh <manyasingh23@yahoo.com>
2024-06-30 00:09:27 +0530

    Create guider.py

---
+++ guider.py
@@ -0,0 +1,8 @@
+processors = ["i9", "i7", "i5"]
+processors_type = ["gaming", "desktop"]
+
+laptops = ["acer", "msi", "asus","hp", "dell"]
+
+desktops = ["lenovo", "hp", "acer", "dell", "asus", "msi"]
+
+brands = ["lenovo", "msi", "acer", "hp", "asus", "dell"]

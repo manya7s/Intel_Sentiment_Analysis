601b5800c657eec08c0347740063ed100dfe5e0a
Manya Singh <manyasingh23@yahoo.com>
2024-06-30 20:08:17 +0530

    deleted guider file

--- guider.py
+++
@@ -1,7 +0,0 @@
-processors = ["i9.txt", "i7.txt", "i5.txt"]
-
-laptops = ["acer.txt", "msi.txt", "asus.txt","hp.txt", "dell.txt"]
-
-desktops = ["lenovo.txt", "hp.txt", "acer.txt", "dell.txt", "asus.txt", "msi.txt"]
-
-brands = ["lenovo.txt", "msi.txt", "acer.txt", "hp.txt", "asus.txt", "dell.txt"]
